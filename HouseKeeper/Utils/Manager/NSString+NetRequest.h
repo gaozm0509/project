e668c9f427625cbd936f0c34116283ce8a0c1868
高泽民 <gaozm@boqii.com>
2016-09-09 08:51:08 +0800

    no message

---
+++ HouseKeeper/Utils/Manager/NSString+NetRequest.h
@@ -0,0 +1,13 @@
+//
+//  NSString+NetRequest.h
+//  HouseKeeper
+//
+//  Created by 高泽民 on 16/9/7.
+//  Copyright © 2016年 zm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (NetRequest)
+
+@end
---
+++ HouseKeeper/Utils/Manager/NSString+NetRequest.m
@@ -0,0 +1,13 @@
+//
+//  NSString+NetRequest.m
+//  HouseKeeper
+//
+//  Created by 高泽民 on 16/9/7.
+//  Copyright © 2016年 zm. All rights reserved.
+//
+
+#import "NSString+NetRequest.h"
+
+@implementation NSString (NetRequest)
+
+@end
